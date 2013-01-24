3d7a14552b5f9306d5af602e424710ea9364d6a0
Maik-Wi <hallo@buntepixel.org>
2013-01-24 13:49:07 -0800

    eine Datei erstellt

---
+++ Bitly.py
@@ -0,0 +1,17 @@
+import clipboard
+import urllib2
+import json
+
+kurzurl = urllib2.quote(clipboard.get())
+
+def shorten_url(long_url):
+     username = 'USERNAME'
+     apikey = 'API-KEY'
+     bitly_url = "http://api.bit.ly/v3/shorten?login=%s&apiKey=%s&longUrl=%s" %(username, apikey, long_url)
+     r = urllib2.Request(bitly_url)
+     opener = urllib2.build_opener()
+     f = json.loads(opener.open(r).read())
+     short_url = f['data']['url']
+     return short_url
+
+print shorten_url(kurzurl)
