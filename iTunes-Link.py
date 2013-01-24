88ae2eae3c65513a26d1d7a5e4a15c6d690f3f5e
Maik-Wi <hallo@buntepixel.org>
2013-01-24 13:49:44 -0800

    eine Datei erstellt

---
+++ iTunes-Link.py
@@ -0,0 +1,19 @@
+#Gebaut für Tradedoubler
+#affID ist alles vor der iTunes-URL
+#affID2 ist alles was nach dem iTunes-Link kommt.
+#Beispiel
+#I===================== affID ============================I          I=== affID2 ==I
+#http://clkde.tradedoubler.com/click?p=23761&a=1998011&url=ITUNES-URL&partnerId=2003
+
+import console
+import urllib
+import clipboard
+
+origURL = clipboard.get()
+
+affID = "http://clkde.tradedoubler.com/click?p=23761&a=1998011&url="
+affID2 = "&partnerId=2003"
+
+affURL = affID + origURL + affID2
+
+clipboard.set(affURL)
