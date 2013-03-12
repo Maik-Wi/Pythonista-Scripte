868b4fffee09dcc4877b4a94f89afa8ff1a72f1a
Maik-Wi <hallo@buntepixel.org>
2013-03-12 09:45:03 +0100

    Auf deutsch bitte
    Dieses Script übersetzt mit Hilfe von Google&nbsp;Translate&nbsp;jedes Wort ins Deutsche, auf Wunsch kann es auch ganze Sätze übersetzten.

---
+++ tranalate.py
@@ -0,0 +1,17 @@
+import urllib2
+import clipboard
+
+def translate(to_translate, to_langage="auto", langage="auto"):
+	agents = {'User-Agent':"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
+	before_trans = 'class="t0">'
+	link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_langage, langage, to_translate.replace(" ", "+"))
+	request = urllib2.Request(link, headers=agents)
+	page = urllib2.urlopen(request).read()
+	result = page[page.find(before_trans)+len(before_trans):]
+	result = result.split("<")[0]
+	return result
+
+if __name__ == '__main__':
+	to_translate = clipboard.get()
+
+	print("%s >> %s" % (to_translate, translate(to_translate, 'de')))
