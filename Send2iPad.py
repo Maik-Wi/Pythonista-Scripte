1b60d5f2ab15ef1c9b428258b9f6eb55e0ef1c85
Maik <hallo@buntepixel.org>
2013-07-26 10:35:10 +0200

    update Send2iPad

--- Send2iPad.py
+++ Send2iPad.py
@@ -12,7 +12,9 @@ print "lade Clipboard..."
 import clipboard
 print "laden der Bibliotheken abgeschlossen"
 
-console.hide_activity()
+# Hier musst du nur deinen User Key eintragen
+userkey = '_DEIN_USER_KEY_'
+
 
 if numArgs < 1:  
     console.show_activity()
@@ -27,7 +29,7 @@ text = console.input_alert('Text', 'Gib hier dein Text ein.')
 conn.request("POST", "/1/messages.json",
 urllib.urlencode({
            "token": "iyQE2RmQZkqBvTSJBxRUYYAedmASXz",
-           "user": "hnkUwTKWYw6HeyyWjoqDq6WiDA5oR9",
+           "user": userkey,
            "message": text,
 }), { "Content-type": "application/x-www-form-urlencoded" })
 conn.getresponse()    
