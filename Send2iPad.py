e556b7e2bfcf5989cc9a954cecf908a29bb3ad21
Maik <hallo@buntepixel.org>
2014-03-23 15:07:18 +0100

    Fix

--- Send2iPad.py
+++ Send2iPad.py
@@ -15,15 +15,12 @@ print "laden der Bibliotheken abgeschlossen"
 # Hier musst du nur deinen User Key eintragen
 userkey = '_DEIN_USER_KEY_'
 
-
-if numArgs < 1:  
-    console.show_activity()
-    text = clipboard.get()
-    console.hide_activity()
-else:
-    text = sys.argv[1]
-    
-  	
+numArgs = len(sys.argv)
+if numArgs == 2:
+	text = sys.argv[1]
+elif numArgs < 2:
+	text = console.input_alert('Text', 'Gib hier dein Text ein.')
+    	
 conn = httplib.HTTPSConnection("api.pushover.net:443")
 text = console.input_alert('Text', 'Gib hier dein Text ein.')
 conn.request("POST", "/1/messages.json",
